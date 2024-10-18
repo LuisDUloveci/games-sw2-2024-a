b643edf0e4b45278b248bb0224fa331c4acfe072
LuisDUloveci <luiskrauzinho@gmail.com>
2024-10-18 12:25:06 +0000

    Aula18_10

--- src/main/java/application/model/Plataforma.java
+++ src/main/java/application/model/Plataforma.java
@@ -7,16 +7,24 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import application.record.PlataformaDTO;
 
 @Entity
 @Table(name = "plataformas")
 @Getter
 @Setter
+@NoArgsConstructor
 public class Plataforma {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(nullable = false, unique = true)
     private String nome;
+
+    public Plataforma(PlataformaDTO dados){
+        this.id = dados.id();
+        this.nome = dados.nome();
+    }
 }
